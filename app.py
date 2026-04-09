f0c261ae7e84743d3959f407ea68db486f1804f9
MostafaAlwakel <mostafaalwkel123@gmail.com>
2026-04-09 17:56:59 +0200

    Force redeploy

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# v2
 from flask import Flask, render_template, redirect, url_for, request, flash, jsonify, session
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import Bcrypt
